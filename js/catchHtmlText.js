c181ca51ec7ab10c85b4a895a68267ba93f6c47a
maksymekgimb <zapasowe.aternos@gmail.com>
2022-02-10 09:05:02 +0100

    10.02.2022

---
+++ js/catchHtmlText.js
@@ -0,0 +1,11 @@
+$(function)() {
+    /*const #lista_ul = $('ul'.html();
+    console.log($lista_ul);
+    const $lista_li=$('li').html();
+    conssole.log($lista_li);*/
+    const $lista_ul = $('ul').text();
+    conssole.log($lista_ul);
+    const $lista_li=$('li').eq(2).text();
+    sonsole.log($lista_li);
+     */
+})
---
+++ js/putAfterAndBefore.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('h2').before('<img src=".../graphics/obraz.png">');
+    $('img').after('<hr><br>');
+})
+//wstawienie przed i po znaczniku
